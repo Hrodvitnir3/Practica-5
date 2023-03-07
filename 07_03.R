840259fbd3e17bdcd1304f5a4efc293fcaf01a62
Hrodvitnir3 <127197761+Hrodvitnir3@users.noreply.github.com>
2023-03-07 12:00:25 +0100

    Update 07_03.R

--- 07_03.R
+++ 07_03.R
@@ -1,1 +1,9 @@
-Practicarstdyget
+#Ejercicio 1
+numArtefactos<-c("17", "54", "10", "34", "90", "33", "49", "82", "12", "23", "56", "78", "44", "102", "10", "53", "4", "28", "37", "95")
+
+#2
+numArtefactos_int<-as.integer(numArtefactos)
+is.integer(numArtefactos_int)
+
+#3
+median(numArtefactos_int)
