66da79f8f7d6687e879e18f7eb50162d455b07b0
Hrodvitnir3 <127197761+Hrodvitnir3@users.noreply.github.com>
2023-03-07 12:08:50 +0100

    Update 07_03.R

--- 07_03.R
+++ 07_03.R
@@ -1,9 +1,16 @@
 #Ejercicio 1
 numArtefactos<-c("17", "54", "10", "34", "90", "33", "49", "82", "12", "23", "56", "78", "44", "102", "10", "53", "4", "28", "37", "95")
 
-#2
+
 numArtefactos_int<-as.integer(numArtefactos)
 is.integer(numArtefactos_int)
+#2
+mean(numArtefactos_int)
 
 #3
 median(numArtefactos_int)
+
+#4
+mfv(numArtefactos_int)
+
+#5
